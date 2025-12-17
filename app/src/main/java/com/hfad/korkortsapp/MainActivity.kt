b38b429891e72e3bb7a5d25a83068be96b9b239b
hedstromfredrik10-a11y <hedstromfredrik10@gmail.com>
2025-12-17 18:13:48 +0100

    Körkortsapp första versionen

--- app/src/main/java/com/hfad/korkortsapp/MainActivity.kt
+++ app/src/main/java/com/hfad/korkortsapp/MainActivity.kt
@@ -1,23 +1,42 @@
 package com.hfad.korkortsapp
 
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.hfad.korkortsapp.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    lateinit var binding: ActivityMainBinding
+    lateinit var quizModelList: MutableList<QuizModel>
+    lateinit var adapter: QuizListAdapter
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
-        }
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
+        quizModelList = mutableListOf()
+        getDataFromFirebase()
 
 
     }
+
+    private fun setUpRecyclerView() {
+        adapter = QuizListAdapter(quizModelList)
+        binding.recyclerView.layoutManager = LinearLayoutManager(this)
+        binding.recyclerView.adapter = adapter
+
+    }
+
+
+    private fun getDataFromFirebase() {
+        quizModelList.add(QuizModel("1", "Skyltar", "Allt om skyltar", "15"))
+        quizModelList.add(QuizModel("2", "Miljö", "Allt om miljö", "15"))
+        quizModelList.add(QuizModel("3", "Bilen", "Allt om bilen", "15"))
+        setUpRecyclerView()
+
+    }
+
+
 }
---
+++ app/src/main/java/com/hfad/korkortsapp/QuizListAdapter.kt
@@ -0,0 +1,41 @@
+package com.hfad.korkortsapp
+
+import android.content.Intent
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.hfad.korkortsapp.databinding.QuizItemRecyclerRowBinding
+import kotlin.jvm.java
+
+class QuizListAdapter(private val quizModelList: List<QuizModel>) :
+    RecyclerView.Adapter<QuizListAdapter.MyViewHolder>() {
+    class MyViewHolder(private val binding: QuizItemRecyclerRowBinding) :
+        RecyclerView.ViewHolder(binding.root) {
+        fun bind(model: QuizModel) {
+            binding.apply {
+                quizTitleText.text = model.title
+                quizSubtitleText.text = model.subtitle
+                quizTimeText.text = model.time + " min"
+                root.setOnClickListener {
+                    val intent = Intent(root.context, QuizActivity::class.java)
+                    root.context.startActivity(intent)
+                }
+            }
+
+        }
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MyViewHolder {
+        val binding =
+            QuizItemRecyclerRowBinding.inflate(LayoutInflater.from(parent.context), parent, false)
+        return MyViewHolder(binding)
+    }
+
+    override fun getItemCount(): Int {
+        return quizModelList.size
+    }
+
+    override fun onBindViewHolder(holder: MyViewHolder, position: Int) {
+        holder.bind(quizModelList[position])
+    }
+}
